b013ed7aef0110def2b785f5907a1bca7d10ac88
digitaldina <dinaelhanan@gmail.com>
2026-01-14 17:34:01 +0400

    comments color change

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,7 +22,7 @@ export const sharedPageComponents: SharedLayout = {
       mapping: 'pathname',
       reactionsEnabled: true,
       inputPosition: 'top',
-      lightTheme: "dark-theme", // corresponds to light-theme.css in quartz/static/giscus/
+      lightTheme: "light-theme", // corresponds to light-theme.css in quartz/static/giscus/
       darkTheme: "dark-theme"
     }
   }),
